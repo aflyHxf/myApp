cf399dea29d22ca76a0b8a25b509e608450f120c
Daly Hu <>
2019-07-23 14:09:36 +0800

    AsyncStorage  替换import 地址  import AsyncStorage from '@react-native-community/async-storage'

--- js/expand/Dao/FavoriteDao.js
+++ js/expand/Dao/FavoriteDao.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import { AsyncStorage } from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage';
 const FAVORITE_KEY_PREFIX = 'favorite_'
 
 export default class FavoriteDao {
